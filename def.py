4c40e84c5f4a684e20a6fd778423b755d443fa04
Ishrat Eliza <37586198+elizaan@users.noreply.github.com>
2021-04-24 15:55:35 +0600

    prediction stalled and so API is replaced

--- def.py
+++ def.py
@@ -25,25 +25,22 @@ from statistics import mean
 import itertools
 import psycopg2
 
+import sys
+import math
 from datetime import datetime as dt, timedelta
 # import plotly.graph_objects as go
-
 url = 'http://coronavirus-tracker-api.herokuapp.com/all'
 url_iso2 = 'https://restcountries.eu/rest/v2/'
-
-# print("hello working")
-
-# %%
-url = 'http://coronavirus-tracker-api.herokuapp.com/all'
-url_iso2 = 'https://restcountries.eu/rest/v2/'
-
 
 response_iso2 = requests.get(url_iso2)
 lst_iso2 = json.loads(response_iso2.text)
 
+
 response = requests.get(url)
 lst = json.loads(response.text)
 
+# response = requests.get(url)
+# lst = json.loads(response.text)
 dict_pop = {}
 dict_iso3 = {}
 dict_affected = {}
@@ -103,7 +100,9 @@ for i in lst['confirmed']['locations']:
             dict_affected[code][j] = dict_affected[code][j] + i['history'][j]
 
 
-with open('country_model_new.csv', 'w') as csv_file:
+
+
+with open('country_model_newApi.csv', 'w', newline='') as csv_file:
     csv_writer = writer(csv_file)
     headers = ['Country_name', 'Country_code_iso2', 'Country_code_iso3',
                'Date', 'Tot_affected', 'Tot_dead', 'Tot_recov', 'Population']
@@ -122,16 +121,13 @@ with open('country_model_new.csv', 'w') as csv_file:
                 #prevDate = s[0] + '/' + str(int(s[1]) - 1) + '/' + s[2]
                 csv_writer.writerow(entry)
 
-# %%
-with open('country_model_new.csv', 'rb') as f:
+# data collection stops; let's import it and save it on X
+with open('country_model_newApi.csv', 'rb') as f:
     result = chardet.detect(f.read())  # or readline if the file is large
 
-dataset = pd.read_csv('country_model_new.csv',
-                      keep_default_na=False,  encoding=result['encoding'])
+dataset = pd.read_csv('country_model_newApi.csv', keep_default_na=False,  encoding=result['encoding'])
 X = dataset.iloc[:, :].values
 
-# %%
-# ----------fixing date (Y-m-d)---------
 for i in range(len(X)):
     # print(i)
     yesterday_str = X[i][3]
@@ -139,18 +135,32 @@ for i in range(len(X)):
     yesterday_str = yesterday.strftime('%Y-%m-%d')
     X[i][3] = yesterday_str
 
+
 temp1 = np.array(np.ones((len(X), 1)))
 temp1 = X[:, 4] - (X[:, 5] + X[:, 6])
 temp1 = temp1.reshape((len(X), 1))
 X = np.hstack((X[:, :7], temp1, X[:, 7:]))
+# from datetime import datetime
+# for date in range(0,len(X)):
+#     X[date][3] = datetime.strptime(X[date][3],"%m/%d/%y")
+#     X[date][3] = X[date][3].strftime( '%m/%d/%y')
+# X[:, 3] = X[:, 3].strftime("%m/%d/%Y")
+print("X er length")
+print(len(X))
+print("X er 4th column e ase ta ki?")
+print(X[len(X)-1][4])
+print(X[len(X)-2][4])
+print("X er 3th column e ase ta ki?")
+print(X[len(X)-1][3])
+print(X[len(X)-2][3])
 
 # %%
 # ------delteing extra data----------
 # from extra, comes extra new datas -_-
 # now let's delete repeating datas
 country_data = (X == "Bangladesh").sum()
-print(country_data)
-# country_data = 105
+#print(country_data)
+
 
 extra = country_data * 230
 X = np.delete(X, slice(extra, len(X)), 0)
@@ -170,7 +180,10 @@ for i in flag:
     X = np.delete(X, slice(i, i+country_data), 0)
 # total country should be 161
 
-# %%
+
+
+#%%
+
 # 3rd part : calculation
 codeList = []
 i = 0
@@ -185,23 +198,14 @@ codeIndex = 1
 report = np.empty((0, 8), object)
 idx = 0
 
-# %%
-# country_data = (X == "Spain").sum()
-# temp1 = np.array(np.ones((len(X),1)))
-# temp1 = X[:,4] - (X[:,5] + X[:,6])
-# temp1 = temp1.reshape((len(X),1))
-# X = np.hstack((X[:, :7], temp1, X[:, 7:]))
-
-# %%
-
-# 3rd part : calculation
-# codeList = []
 # i = 0
-# t = dt.today() - timedelta(days = 1)
-# t = t.strftime("%Y-%m-%d")
+# t = dt.today() - timedelta(days=1)
+# t = t.strftime("%m/%d/%y")
+# print("t er value ki?")
+# print(t)
 # while i < len(X):
 #     #40 threshold set as total cases to avoid unexpected results
-#     if X[i][3] == t and X[i][4] >= 100:
+#     if X[i][3] == t and X[i][4] >= 40:
 #         codeList.append(X[i][1])
 #     i = i + 1
 # # print(len(codeList) == len(set(codeList)))
@@ -211,6 +215,8 @@ idx = 0
 
 
 code = 'IN'
+print("codelist er lengh")
+print(len(codeList))
 
 t = dt.today() - timedelta(days=1)
 t = t.strftime("%Y-%m-%d")
@@ -229,9 +235,9 @@ for code in itertools.islice(codeList, 0, len(codeList)):
         if row[codeIndex] == code and row[3] <= t:
             arr[datas] = row
             datas = datas + 1
-    # print(arr[0][0])
-    arr = arr[np.argsort(arr[:, 3])]
+    #print(arr[0][0])
 
+    arr = arr[np.argsort(arr[:, 3])]
     # init: previous values (0 for dead, 1 for affected)
     init = np.empty((datas, 2), dtype=object)
     init[0][0] = arr[0][5]
@@ -253,12 +259,55 @@ for code in itertools.islice(codeList, 0, len(codeList)):
                 if arr[i][7] != 0:  # divide by zero!
                     l = (arr[i+1][6] - arr[i][6]) / \
                         arr[i][7]  # lamdaR=(R1 - R0)/ Ir0
-            if arr[i][3] >= "2020-03-05":  # timing considered from March 05 to see its growing shape
+            if arr[i][3] >= "2020-03-05":
                 if l < 0:
-                    l = 0  # to avoid unpredented results
+                    l = 0  # timing considered from March 05 to see its growing shape
                 lst.append(l)
         return lst
 
+    """"lamdaD = calLamda(True)
+    lamdaR = calLamda(False)
+    # last 5 values average taken as constant
+    ld = mean(lamdaD[-5:])
+    # print(ld)
+    lr = mean(lamdaR[-5:])
+    # print(lr)    
+    
+    lstBSbyL = []
+    for i in range(len(arr) - 1):
+        #BS0 = ((Id1 + Ir1 + Id0*lamdaD + Ir0*lamdaR) - (Id0 + Ir0)) / (Id0 + Ir0)
+        l = 0
+        fplus = arr[i+1][7] + arr[i][7]*(ld + lr)
+        splus = arr[i][7]
+        if splus != 0:
+            l = (fplus - splus)/ splus    
+        if arr[i][3] >= "2020-03-05":                           #timing considered from March 05 to see its growing shape
+            lstBSbyL.append(l)            
+    # last 5 values average taken as constant
+    lbsl = mean(lstBSbyL[-5:])
+    # print(lbsl/ (ld + lr))
+    
+    # pop: population of corresponding country
+    for i in range(len(X)):
+        if X[i][codeIndex] == code:
+            pop = X[i][8]
+            # print(pop)
+            break
+    # number of confirmed cases subtracted from actual population
+    pop = pop - arr[-1][4]
+    # now some hardcoding max, min, average bullshit begins!!
+    minConst = 50
+    maxConst = 1000
+    midConst = 300
+    pop1 = pop / minConst
+    beta1 = (lbsl / pop1)
+    
+    pop3 = pop / maxConst
+    beta3 = (lbsl / pop3)
+    
+    pop2 = pop / midConst
+    beta2 = (lbsl / pop2)"""
+    ##hishu hospital theke dise
     lamdaD = calLamda(True)
     lamdaR = calLamda(False)
     days_to_take = 8
@@ -302,16 +351,14 @@ for code in itertools.islice(codeList, 0, len(codeList)):
     # here goes our incremental SIRD model; (Eliza, try an ODE version? I haven't tried)
     # this function returns future daily affecteds or deads
     def calcDiff(pop, beta):
-        # print("pop,beta,array[-1][7]" )
-        # print(pop,beta,arr[-1][7])
-        final = np.empty((360, 4), dtype=object)
-        lame = np.empty(360, dtype=object)
-        lame2 = np.empty(360, dtype=object)
+        final = np.empty((30, 4), dtype=object)
+        lame = np.empty(30, dtype=object)
+        lame2 = np.empty(30, dtype=object)
         final[0][0] = pop - beta*pop*arr[-1][7]  # S
         final[0][1] = arr[-1][7] + beta*pop * \
             arr[-1][7] - (ld+lr)*arr[-1][7]  # I
         final[0][2] = arr[-1][6] + lr*arr[-1][7]  # R
-        final[0][3] = arr[-1][5] + ld*arr[-1][7]  # D
+        final[0][3] = arr[-1][5] + ld*arr[-1][7]
 
         for i in range(1, len(final)):
             final[i][0] = final[i-1][0] - beta*final[i-1][0]*final[i-1][1]
@@ -319,9 +366,19 @@ for code in itertools.islice(codeList, 0, len(codeList)):
                 final[i-1][1] - (ld+lr)*final[i-1][1]
             final[i][2] = final[i-1][2] + lr*final[i-1][1]
             final[i][3] = final[i-1][3] + ld*final[i-1][1]
-            # print(final[i][0],final[i][1],final[i][2],final[i][3])
+            if final[i][0] > sys.maxsize or final[i][1] > sys.maxsize or final[i][2] > sys.maxsize or final[i][3] > sys.maxsize:
+                final[i][0] = 0
+                final[i][1] = 0
+                final[i][2] = 0
+                final[i][3] = 0
+            if np.isnan(final[i][0]) or np.isnan(final[i][1]) or np.isnan(final[i][2]) or np.isnan(final[i][3]) or math.isinf(final[i][0]) or math.isinf(final[i][1]) or math.isinf(final[i][2]) or math.isinf(final[i][3]):
 
-        final[:, [0, 1, 2, 3]] = final[:, [0, 1, 2, 3]].astype(int)
+                final[i][0] = 0
+                final[i][1] = 0
+                final[i][2] = 0
+                final[i][3] = 0
+
+        final[:, [0, 1, 2, 3]] = final[:, [0, 1, 2, 3]].astype('int')
         final = np.append(final, np.ones((len(final), 1)), axis=1)
         final[:, 4] = final[:, 1] + final[:, 2] + \
             final[:, 3]  # cases=active+R+D
@@ -329,23 +386,20 @@ for code in itertools.islice(codeList, 0, len(codeList)):
         diffDead = []
         diffDead.append(abs(final[0][3] - arr[-1][5]))
         for i in range(1, len(final)):
-
             if (final[i][3] - final[i-1][3]) < 0:
                 lame[i] = final[i][3] - final[i-1][3]
                 lame[i] = 0
                 diffDead.append(lame[i])
-
             else:
                 diffDead.append(final[i][3] - final[i-1][3])
+
         diffAffec = []
         diffAffec.append(abs(final[0][4] - arr[-1][4]))
         for i in range(1, len(final)):
-
             if (final[i][4] - final[i-1][4]) < 0:
                 lame2[i] = final[i][4] - final[i-1][4]
                 lame2[i] = 0
                 diffAffec.append(lame2[i])
-
             else:
                 diffAffec.append(final[i][4] - final[i-1][4])
 
@@ -355,116 +409,211 @@ for code in itertools.islice(codeList, 0, len(codeList)):
     dA2, dD2 = calcDiff(pop2, beta2)
     dA3, dD3 = calcDiff(pop3, beta3)
 
-    # if code == 'BD':
-    #     print(code, dA1, dA2, dA3)
-
-    # ploting stuffs
+    """    
+    #ploting stuffs
     # init[:,0]-> dead, 1-> affected; change dA-> for dead
-    prevVals = init[:, 1]
-#     x = np.arange(datas, datas+360)
-#     plt.plot(prevVals, color = 'b')
-#     plt.plot(x,dA1, '--', color = 'r')
-#     plt.plot(x,dA2, '--', color = 'black')
-#     plt.plot(x,dA3, '--', color = 'r')
-#     plt.title(f"{arr[0][0]}")
-#     plt.show()
-    # arr = np.flip(arr,0)
-
+    prevVals = init[:,1]
+    x = np.arange(datas, datas+30) 
+    plt.plot(prevVals, color = 'b')
+    plt.plot(x,dA1, '--', color = 'r')    
+    plt.plot(x,dA2, '--', color = 'black')
+    plt.plot(x,dA3, '--', color = 'r')
+    plt.title(f"{arr[0][0]}")
+    plt.show()
+    """
     # now lets create our report, which will be pushed into database
     name = arr[0][0]
-    name = np.full((360, 1), name)
+    name = np.full((30, 1), name)
     code2 = arr[0][1]
-    code2 = np.full((360, 1), code2)
+    code2 = np.full((30, 1), code2)
 
     one_day = timedelta(days=1)
     yesterday_str = arr[-1][3]
     yesterday = dt.strptime(yesterday_str, '%Y-%m-%d').date()
-    Today = np.full((360, 1), yesterday_str)
+    Today = np.full((30, 1), yesterday_str)
 
     day_predict = []
-    for i in range(360):
+    for i in range(30):
         day = yesterday + one_day
         yesterday = day
         day = day.strftime("%Y-%m-%d")
         day_predict.append(day)
     day_predict = np.array(day_predict)
-    day_predict = day_predict.reshape((360, 1))
+    day_predict = day_predict.reshape((30, 1))
 
     def createReport(string, v1, v2, v3):
-        Type = np.full((360, 1), string)
+        Type = np.full((30, 1), string)
         v1 = np.array(v1)
         v2 = np.array(v2)
         v3 = np.array(v3)
-        v1 = v1.reshape((360, 1))
-        v2 = v2.reshape((360, 1))
-        v3 = v3.reshape((360, 1))
+        v1 = v1.reshape((30, 1))
+        v2 = v2.reshape((30, 1))
+        v3 = v3.reshape((30, 1))
         values = np.hstack((Type, name, code2, Today, day_predict, v2, v1, v3))
-        values = values.reshape((360, 8))
+        values = values.reshape((30, 8))
         return values
 
     reportAffec = createReport("Affected", dA1, dA2, dA3)
     reportDead = createReport("Dead", dD1, dD2, dD3)
     report = np.vstack((report[:idx, :], reportAffec,
-                        reportDead, report[idx+720:, :]))
-    idx = idx + 720
+                       reportDead, report[idx+60:, :]))
+    idx = idx + 60
+noCountries = int(len(report)/60)
 
-# report
-# 0-> type (affected/ dead)
-# 1-> country_name
-# 2-> code2
-# 3-> database pushing date
-# 4-> prediction date
-# 5-> middle val
-# 6-> max val
-# 7-> min val
-# """
+"""
+report
+0-> type (affected/ dead)
+1-> country_name
+2-> code2
+3-> database pushing date
+4-> prediction date
+5-> middle val
+6-> max val
+7-> min val
+"""
 
-# #%%
-# %%
-noCountries = int(len(report)/720)
-noCountries
+#%%
 
-print(len(report))
+tot_countries = noCountries
+dbase = np.empty(((tot_countries, 11)), dtype=object)
+print("length of report and codelist")
+print(len(report)/60)
+print(len(codeList))
 
-for k in range(len(report)):
-    if report[k][1] == "Côte d'Ivoire":
+names = []
 
-        report[k][1] = "Cote dIvoire"
+for i in range(len(X)-1):
+    for j in range(len(codeList)):
+        if codeList[j] == X[i][1]:
+            dbase[j][0] = X[i][0]
+            dbase[j][1] = X[i][2]
+            dbase[j][2] = t
+            if X[i][3] == t:
+                dbase[j][3] = X[i][4] - X[i+1][4]
+                dbase[j][4] = X[i][5] - X[i+1][5]
+        j = j+1
+    i = i+1
 
-with open("data.sql", "w") as sql_file:
-    for j in range(len(report)):
-        sql_file.write("INSERT INTO longtermprediction(region_id,alpha2,type,insertion_date,prediction_date,median,maximum,minimum,name)VALUES((select id from regions where alpha2 = '%s'LIMIT 1),'%s','%s','%s','%s',%s,%s,%s,'%s');\n" % (
-            report[j][2], report[j][2], report[j][0], report[j][3], report[j][4], report[j][5], report[j][6], report[j][7], report[j][1]))
+#print(dbase)
+# j = 0
+# i = country_data-1
+# for j in range(noCountries):
+##     # print(i)
+# #     if X[i][0] == "Serbia" or X[i][0] == "Antigua and Barbuda":
+# #         del X[i][0]
+# #         del X[i][2]
+# #         del X[i][3]
+# #         del X[i][4]
+# #         del X[i][5]
+
+# #         X[i][0] = X[i+1][0]
+# #         X[i][2] = X[i+1][2]
+# #         X[i][3] = X[i+1][3]
+# #         X[i][4] = X[i+1][4]
+# #         X[i][5] = X[i+1][5]
+
+#     dbase[j][0] = X[i][0]
+#     dbase[j][1] = X[i][2]
+#     dbase[j][2] = t
+#     dbase[j][3] = X[i][4] - X[i-1][4]
+#     dbase[j][4] = X[i][5] - X[i-1][5]
+#     j = j+1
+#     i = i + country_data
+i = 0
+for j in range(noCountries):
+    dbase[j][5] = report[i][4]
+    dbase[j][6] = report[i][4+2]
+    dbase[j][7] = report[i+30][4+2]
+    dbase[j][8] = report[i+1][4]
+    dbase[j][9] = report[i+1][4+2]
+    dbase[j][10] = report[i+30+1][4+2]
+    i = i+60
+
+#print(dbase)
+
+"""
+dbase[:,0] = country
+dbase[:,1] = code
+dbase[:,2] = date_yesterday
+dbase[:,3] = affected_yesterday
+dbase[:,4] = death_yesterday
+dbase[:,5] = date_today
+dbase[:,6] = affected_today
+dbase[:,7] = death_today
+dbase[:,8] = date_tomorrow
+dbase[:,9] = affected_tomorrow
+dbase[:,10] = death_tomorrow
+"""
+
+#%%
 
 
-sql_file = open("data.sql", "r")
+def getRegion(regID):
+    try:
 
-try:
-    connection = psycopg2.connect(
-        "dbname='postgres' user='admin_corona@corona-forecast' host='corona-forecast.postgres.database.azure.com' port='5432' password='T0wardsResolution'")
-    cursor = connection.cursor()
-
-    # Print PostgreSQL Connection properties
-    print(connection.get_dsn_parameters(), "\n")
+        # Print PostgreSQL Connection properties
+        print(connection.get_dsn_parameters(), "\n")
 
     # Print PostgreSQL version
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record, "\n")
+        cursor.execute("SELECT version();")
+        record = cursor.fetchone()
+        print("You are connected to - ", record, "\n")
 
-    cursor.execute(sql_file.read())
+        postgres_select_query = "select id from regions where alpha3 = %s"
 
-    connection.commit()
-    count = cursor.rowcount
-    print(count, "Record inserted successfully into worldprediction table")
+        cursor.execute(postgres_select_query, (regID,))
 
-except (Exception, psycopg2.Error) as error:
-    if(connection):
-        print("Failed to insert record into worldprediction table", error)
+        reg = cursor.fetchall()
+        for r in reg:
+            print(r[0])
+            return r[0]
+        #print(reg)
 
-finally:
-    # closing database connection.
-    if(connection):
-        cursor.close()
-        connection.close()
+       # connection.commit()
+       # count = cursor.rowcount
+       # print (count, "Record inserted successfully into worldprediction table")
+
+    except (Exception, psycopg2.Error) as error:
+        if(connection):
+            print("Error fetching data from PostgreSQL table", error)
+
+    #return reg[0]
+
+
+# In[11]:
+
+
+def insertInfo(regid, alpha3, name, date, affected_1, death_1, affected_2, death_2, affected_3, death_3, pred):
+    try:
+        name = name.replace("'", "''")
+        postgres_insert_query = """INSERT INTO countryprediction(region_id,alpha_3,country_name, date , affected_1, death_1 , affected_2, death_2 , affected_3, death_3 , prediction_calculation)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+
+        cursor.execute(postgres_insert_query, (regid, alpha3, name, date,
+                       affected_1, death_1, affected_2, death_2, affected_3, death_3, pred))
+
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Record inserted successfully into countryprediction table")
+
+    except (Exception, psycopg2.Error) as error:
+        if(connection):
+            print("Error inserting data in PostgreSQL table", error)
+
+
+# In[12]:
+connection = psycopg2.connect(
+    "dbname='postgres' user='admin_corona@corona-forecast' host='corona-forecast.postgres.database.azure.com' port='5432' password='T0wardsResolution'")
+cursor = connection.cursor()
+
+regid = []
+for i in range(tot_countries):
+    regid = getRegion(dbase[i][1])
+    insertInfo(regid, dbase[i][1], dbase[i][0], dbase[i][2], dbase[i][3],
+               dbase[i][4], dbase[i][6], dbase[i][7], dbase[i][9], dbase[i][10], 0)
+    print(i)
+
+#closing connection
+if(connection):
+    cursor.close()
+    connection.close()
     print("PostgreSQL connection is closed")
